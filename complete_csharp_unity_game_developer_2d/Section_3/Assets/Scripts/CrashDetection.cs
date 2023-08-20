b42cdf5246d7502f9fa896452d80653d26acca0d
cvera-tech <25487412+cvera-tech@users.noreply.github.com>
2023-08-20 16:27:17 -0400

    Complete Lecture 39
    Added delay to the scene reload.

--- complete_csharp_unity_game_developer_2d/Section_3/Assets/Scripts/CrashDetection.cs
+++ complete_csharp_unity_game_developer_2d/Section_3/Assets/Scripts/CrashDetection.cs
@@ -3,12 +3,19 @@ using UnityEngine.SceneManagement;
 
 public class CrashDetection : MonoBehaviour
 {
+    [SerializeField] float reloadSceneDelay = 1f;
+    
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Ground"))
         {
-            SceneManager.LoadScene("Level1");
             Debug.Log("deudly ice my doge");
+            Invoke("ReloadScene", reloadSceneDelay);
         }
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene("Level1");
     }
 }
--- complete_csharp_unity_game_developer_2d/Section_3/Assets/Scripts/FinishLine.cs
+++ complete_csharp_unity_game_developer_2d/Section_3/Assets/Scripts/FinishLine.cs
@@ -1,10 +1,10 @@
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
     // [SerializeField] GameObject startPositionObject;
+    [SerializeField] float reloadSceneDelay = 1f;
 
     void OnTriggerEnter2D(Collider2D other)
     {
@@ -15,7 +15,12 @@ public class FinishLine : MonoBehaviour
             // player.transform.SetPositionAndRotation(startPosition, startRotation);
             // player.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
             // player.GetComponent<Rigidbody2D>().angularVelocity = 0;
-            SceneManager.LoadScene("Level1");
+            Invoke("ReloadScene", reloadSceneDelay);
         }
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene("Level1");
     }
 }
