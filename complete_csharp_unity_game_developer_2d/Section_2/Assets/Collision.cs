50c68eb5302fd7fae868e83d458788bfc9ba2be3
cvera-tech <25487412+cvera-tech@users.noreply.github.com>
2023-08-19 21:24:31 -0400

    Complete Lecture 19
    Add trigger method to Collision component.

--- complete_csharp_unity_game_developer_2d/Section_2/Assets/Collision.cs
+++ complete_csharp_unity_game_developer_2d/Section_2/Assets/Collision.cs
@@ -5,4 +5,8 @@ public class Collision : MonoBehaviour
     void OnCollisionEnter2D() {
         Debug.Log("Oof.");
     }
+
+    void OnTriggerEnter2D() {
+        Debug.Log("Rough ride!");
+    }
 }
