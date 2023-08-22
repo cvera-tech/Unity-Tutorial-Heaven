78aae42bf06df334d678f199495095901bc5b755
cvera-tech <25487412+cvera-tech@users.noreply.github.com>
2023-08-21 21:43:47 -0400

    Complete Lecture 58
    Added a loop to fill in all the button texts.

--- complete_csharp_unity_game_developer_2d/Section_4/Assets/Scripts/QuizController.cs
+++ complete_csharp_unity_game_developer_2d/Section_4/Assets/Scripts/QuizController.cs
@@ -6,9 +6,17 @@ public class QuizController : MonoBehaviour
 
     [SerializeField] TextMeshProUGUI questionText;
     [SerializeField] QuestionSO question;
+    [SerializeField] GameObject[] answerButtons;
 
     public void Start()
     {
         questionText.text = question.Question;
+
+        for (int i = 0; i < answerButtons.Length; i++) 
+        {
+            GameObject answerButton = answerButtons[i];
+            TextMeshProUGUI buttonTMP = answerButton.GetComponentInChildren<TextMeshProUGUI>();
+            buttonTMP.text = question.GetAnswer(i);
+        }
     }
 }
