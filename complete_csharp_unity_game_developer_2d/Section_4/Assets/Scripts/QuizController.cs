9f56f21672f01c1302d7cd1e28f2ac3824b0f632
cvera-tech <25487412+cvera-tech@users.noreply.github.com>
2023-08-21 22:14:01 -0400

    Complete Lecture 59
    Added feature to highlight correct and wrong answers.

--- complete_csharp_unity_game_developer_2d/Section_4/Assets/Scripts/QuizController.cs
+++ complete_csharp_unity_game_developer_2d/Section_4/Assets/Scripts/QuizController.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class QuizController : MonoBehaviour
 {
@@ -7,16 +8,43 @@ public class QuizController : MonoBehaviour
     [SerializeField] TextMeshProUGUI questionText;
     [SerializeField] QuestionSO question;
     [SerializeField] GameObject[] answerButtons;
+    [SerializeField] Sprite correctAnswerSprite;
+    [SerializeField] Sprite wrongAnswerSprite;
+    [SerializeField] string correctText = "Correct!";
+    [SerializeField] string wrongText = "Wrong!";
 
-    public void Start()
+    private int correctAnswerIndex;
+
+    void Start()
     {
         questionText.text = question.Question;
+        correctAnswerIndex = question.CorrectAnswerIndex;
 
         for (int i = 0; i < answerButtons.Length; i++) 
         {
             GameObject answerButton = answerButtons[i];
             TextMeshProUGUI buttonTMP = answerButton.GetComponentInChildren<TextMeshProUGUI>();
             buttonTMP.text = question.GetAnswer(i);
+        }
+    }
+
+    public void OnAnswerSelect(int index)
+    {
+        GameObject selectedButton = answerButtons[index];
+        Image buttonImage = selectedButton.GetComponent<Image>();
+        
+        if (index == correctAnswerIndex)
+        {
+            questionText.text = correctText;
+            buttonImage.sprite = correctAnswerSprite;
+        }
+        else
+        {
+            questionText.text = wrongText;
+            buttonImage.sprite = wrongAnswerSprite;
+            GameObject correctButton = answerButtons[correctAnswerIndex];
+            Image correctImage = correctButton.GetComponent<Image>();
+            correctImage.sprite = correctAnswerSprite;
         }
     }
 }
