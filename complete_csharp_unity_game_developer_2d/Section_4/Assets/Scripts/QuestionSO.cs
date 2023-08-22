52b45075ae187e77688e18fe118ef4f750d0dfd3
cvera-tech <25487412+cvera-tech@users.noreply.github.com>
2023-08-21 21:20:12 -0400

    Complete Lectures 55 and 56
    Added answers to the Question scriptable object and filled in the answer values of the question assets.

--- complete_csharp_unity_game_developer_2d/Section_4/Assets/Scripts/QuestionSO.cs
+++ complete_csharp_unity_game_developer_2d/Section_4/Assets/Scripts/QuestionSO.cs
@@ -4,5 +4,21 @@ using UnityEngine;
 public class QuestionSO : ScriptableObject
 {
     [SerializeField, TextArea(2, 6)]
-    string question = "Enter question text here.";
+    string _question = "Enter question text here.";
+
+    [SerializeField]
+    string[] _answers = new string[4];
+
+    [SerializeField]
+    int _correctAnswerIndex;
+
+    public int CorrectAnswerIndex { get => _correctAnswerIndex; }
+
+    public string Question { get => _question; }
+    
+    public string GetAnswer (int index)
+    {
+        return _answers[index];
+    }
+    
 }
