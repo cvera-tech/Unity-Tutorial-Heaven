821beae4bd297641503c8bc7824d387ef6eb2691
cvera-tech <25487412+cvera-tech@users.noreply.github.com>
2023-08-26 17:05:30 -0400

    Complete Lecture 86
    Added ladder climbing.

--- complete_csharp_unity_game_developer_2d/Section_5/Assets/Scripts/PlayerMovement.cs
+++ complete_csharp_unity_game_developer_2d/Section_5/Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,5 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -11,10 +13,17 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float runSpeed = 1f;
     [SerializeField] private bool facingRight = true;
     [SerializeField] private float jumpSpeed = 5f;
+    [SerializeField] private float climbSpeed = 5f;
+
+    private float defaultGravityScale;
+
+
+    // private bool isTouchingLadder = false;
     
     // TODO: Extract these magic strings to their own class
     // Perhaps a scriptable object?
     private readonly string isRunning = "isRunning";
+    private readonly string ladderLayer = "Ladder";
     private readonly string groundLayer = "Ground";
 
     void Start()
@@ -22,12 +31,23 @@ public class PlayerMovement : MonoBehaviour
         rb2d = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         cc2d = GetComponent<CapsuleCollider2D>();
+
+        defaultGravityScale = rb2d.gravityScale;
     }
 
     void Update()
     {
         Run();
         FlipSprite();
+        ClimbLadder();
+    }
+
+    private void OnJump(InputValue value)
+    {
+        if (value.isPressed && IsTouchingLayer(groundLayer))
+        {
+            rb2d.velocity += new Vector2(0f, jumpSpeed);
+        }
     }
 
     private void OnMove(InputValue value)
@@ -37,6 +57,29 @@ public class PlayerMovement : MonoBehaviour
             facingRight = moveInput.x > Mathf.Epsilon;
     }
 
+    /*
+        Custom methods
+    */
+
+    private void ClimbLadder()
+    {
+        if (IsTouchingLayer(ladderLayer))
+        {
+            rb2d.gravityScale = 0;
+            if (Mathf.Abs(moveInput.y) > Mathf.Epsilon)
+            {
+                rb2d.velocity = new Vector2(rb2d.velocity.x, moveInput.y * climbSpeed);
+            }
+            else
+            {
+                rb2d.velocity = new Vector2(rb2d.velocity.x, 0f);
+            }
+        }
+        else
+            rb2d.gravityScale = defaultGravityScale;
+
+        
+    }
 
     private void FlipSprite()
     {
@@ -44,14 +87,9 @@ public class PlayerMovement : MonoBehaviour
     }
 
     private bool HasHorizontalMoveInput() => Mathf.Abs(moveInput.x) > Mathf.Epsilon;
-    
-    private void OnJump(InputValue value)
-    {
-        if (value.isPressed && cc2d.IsTouchingLayers(LayerMask.GetMask(groundLayer)))
-        {
-            rb2d.velocity += new Vector2(0f, jumpSpeed);
-        }
-    }
+
+    private bool IsTouchingLayer(string layerMaskString) => cc2d.IsTouchingLayers(LayerMask.GetMask(layerMaskString));
+
     private void Run()
     {
         Vector2 runVelocity = new(moveInput.x * runSpeed, rb2d.velocity.y);
@@ -59,5 +97,7 @@ public class PlayerMovement : MonoBehaviour
         
         animator.SetBool(isRunning, HasHorizontalMoveInput());
     }
+
+    
 
 }
