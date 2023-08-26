8fec0f3a63509e0b93a3e55ded3b8498d1282c83
cvera-tech <25487412+cvera-tech@users.noreply.github.com>
2023-08-26 14:02:13 -0400

    Complete Lecture 81
    Added horizontal movement for the player.

--- complete_csharp_unity_game_developer_2d/Section_5/Assets/Scripts/PlayerMovement.cs
+++ complete_csharp_unity_game_developer_2d/Section_5/Assets/Scripts/PlayerMovement.cs
@@ -4,20 +4,29 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     private Vector2 moveInput;
+    private Rigidbody2D rb2d;
+
+    [SerializeField] private float runSpeed = 1f;
 
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        
+        Run();
     }
 
     private void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
         Debug.Log(moveInput);
+    }
+
+    private void Run()
+    {
+        Vector2 runVelocity = new(moveInput.x * runSpeed, rb2d.velocity.y);
+        rb2d.velocity = runVelocity;
     }
 }
