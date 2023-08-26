9d419cac5d9f61c9ad49c900733b24075fa8f3b8
cvera-tech <25487412+cvera-tech@users.noreply.github.com>
2023-08-26 15:11:37 -0400

    Complete Lecture 83
    Added programmatic running animation activation.

--- complete_csharp_unity_game_developer_2d/Section_5/Assets/Scripts/PlayerMovement.cs
+++ complete_csharp_unity_game_developer_2d/Section_5/Assets/Scripts/PlayerMovement.cs
@@ -5,13 +5,19 @@ public class PlayerMovement : MonoBehaviour
 {
     private Vector2 moveInput;
     private Rigidbody2D rb2d;
+    private Animator animator;
 
     [SerializeField] private float runSpeed = 1f;
     [SerializeField] private bool facingRight = true;
+    
+    // TODO: Extract these magic strings to their own class
+    // Perhaps a scriptable object?
+    private readonly string isRunning = "isRunning";
 
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
@@ -23,19 +29,23 @@ public class PlayerMovement : MonoBehaviour
     private void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
-        if (Mathf.Abs(moveInput.x) > Mathf.Epsilon)
+        if (HasHorizontalMoveInput())
             facingRight = moveInput.x > Mathf.Epsilon;
-        Debug.Log("facingRight: " + facingRight);
     }
 
-    private void Run()
-    {
-        Vector2 runVelocity = new(moveInput.x * runSpeed, rb2d.velocity.y);
-        rb2d.velocity = runVelocity;
-    }
 
     private void FlipSprite()
     {
         transform.localScale = new Vector2(facingRight ? 1f : -1f, 1f);
     }
+    
+    private void Run()
+    {
+        Vector2 runVelocity = new(moveInput.x * runSpeed, rb2d.velocity.y);
+        rb2d.velocity = runVelocity;
+        
+        animator.SetBool(isRunning, HasHorizontalMoveInput());
+    }
+
+    private bool HasHorizontalMoveInput() => Mathf.Abs(moveInput.x) > Mathf.Epsilon;
 }
