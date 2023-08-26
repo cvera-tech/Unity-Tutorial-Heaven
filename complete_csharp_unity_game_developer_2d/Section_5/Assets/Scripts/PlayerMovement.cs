72aa0edd35108d0954e15001ec1a1ec2367c66b2
cvera-tech <25487412+cvera-tech@users.noreply.github.com>
2023-08-26 14:33:23 -0400

    Complete Lecture 82
    Added sprite flipping depending on which move button was last pressed.

--- complete_csharp_unity_game_developer_2d/Section_5/Assets/Scripts/PlayerMovement.cs
+++ complete_csharp_unity_game_developer_2d/Section_5/Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,7 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb2d;
 
     [SerializeField] private float runSpeed = 1f;
+    [SerializeField] private bool facingRight = true;
 
     void Start()
     {
@@ -16,17 +17,25 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         Run();
+        FlipSprite();
     }
 
     private void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
-        Debug.Log(moveInput);
+        if (Mathf.Abs(moveInput.x) > Mathf.Epsilon)
+            facingRight = moveInput.x > Mathf.Epsilon;
+        Debug.Log("facingRight: " + facingRight);
     }
 
     private void Run()
     {
         Vector2 runVelocity = new(moveInput.x * runSpeed, rb2d.velocity.y);
         rb2d.velocity = runVelocity;
+    }
+
+    private void FlipSprite()
+    {
+        transform.localScale = new Vector2(facingRight ? 1f : -1f, 1f);
     }
 }
