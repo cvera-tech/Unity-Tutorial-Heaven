3dead2c08f20501c4471c32f37ceb51b43bf0dfd
cvera-tech <25487412+cvera-tech@users.noreply.github.com>
2023-08-29 22:31:00 -0400

    Complete Lecture 105
    Added a coin pickup sound effect.

--- complete_csharp_unity_game_developer_2d/Section_5/Assets/Scripts/CoinPickup.cs
+++ complete_csharp_unity_game_developer_2d/Section_5/Assets/Scripts/CoinPickup.cs
@@ -2,8 +2,15 @@ using UnityEngine;
 
 public class CoinPickup : MonoBehaviour, Interactable
 {
+    [SerializeField] private AudioClip _audioClip;
+
+    // TODO: Convert this to an event-based system
     public void Interact()
     {
+        Vector3 coinPosition = gameObject.transform.position;
+        AudioSource.PlayClipAtPoint(_audioClip, coinPosition);
+        // Vector3 audioPosition = new Vector3(coinPosition.x, coinPosition.y, Camera.main.transform.position.z);
+        // AudioSource.PlayClipAtPoint(_audioClip, audioPosition);
         Destroy(gameObject);
     }
 }
