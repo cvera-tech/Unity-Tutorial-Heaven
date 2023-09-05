b965a3c566747d5804692c670a964fc6d1a7742c
cvera-tech <25487412+cvera-tech@users.noreply.github.com>
2023-09-05 19:23:55 -0400

    Complete Lecture 122
    Added player shooting.

--- complete_csharp_unity_game_developer_2d/Section_6/Assets/Scripts/Player/PlayerController.cs
+++ complete_csharp_unity_game_developer_2d/Section_6/Assets/Scripts/Player/PlayerController.cs
@@ -4,42 +4,55 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
 
-    [SerializeField] private float moveSpeed = 10f;
+    [SerializeField] private float _moveSpeed = 10f;
+    [SerializeField] private float _paddingLeft;
+    [SerializeField] private float _paddingRight;
+    [SerializeField] private float _paddingTop;
+    [SerializeField] private float _paddingBottom;
 
-    private Vector2 moveInput;
-    private Vector2 minBounds;
-    private Vector2 maxBounds;
 
-    [SerializeField] private float paddingLeft;
-    [SerializeField] private float paddingRight;
-    [SerializeField] private float paddingTop;
-    [SerializeField] private float paddingBottom;
+    private Vector2 _moveInput;
+    private Vector2 _minBounds;
+    private Vector2 _maxBounds;
 
+    private Shooter _shooter;
 
     private void Start()
     {
         InitializeBounds();
+        _shooter = GetComponent<Shooter>();
     }
     private void Update()
     {
-        Vector2 calculatedPosition = moveSpeed * Time.deltaTime * moveInput;
+        Vector2 calculatedPosition = _moveSpeed * Time.deltaTime * _moveInput;
         Vector2 newPosition = new()
         {
             // Note: These are in world space units, not pixels!
-            x = Mathf.Clamp(transform.position.x + calculatedPosition.x, minBounds.x + paddingLeft, maxBounds.x - paddingRight),
-            y = Mathf.Clamp(transform.position.y + calculatedPosition.y, minBounds.y + paddingBottom, maxBounds.y - paddingTop)
+            x = Mathf.Clamp(transform.position.x + calculatedPosition.x, _minBounds.x + _paddingLeft, _maxBounds.x - _paddingRight),
+            y = Mathf.Clamp(transform.position.y + calculatedPosition.y, _minBounds.y + _paddingBottom, _maxBounds.y - _paddingTop)
         };
         transform.position = newPosition;
     }
     private void OnMove(InputValue inputValue)
     {
-        moveInput = inputValue.Get<Vector2>();
+        _moveInput = inputValue.Get<Vector2>();
     }
+
+    private void OnFire(InputValue inputValue)
+    {
+        if (_shooter != null)
+        {
+            Debug.Log("Firing! " + inputValue.isPressed);
+            _shooter.IsFiring = inputValue.isPressed;
+        }
+    }
+
     private void InitializeBounds()
     {
-        minBounds = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
-        maxBounds = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+        _minBounds = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+        _maxBounds = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
         // Debug.Log("Min = " + minBounds.ToString());
         // Debug.Log("Max = " + maxBounds.ToString());
     }
+
 }
